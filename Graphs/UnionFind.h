2405098d6e31dabc7851c558f1924e26d6f36c58
Jakob Kogler <jakob.kogler@gmail.com>
2017-10-03 17:14:55 +0200

    UnionFind implementation

---
+++ Graphs/UnionFind.h
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <vector>
+
+class UnionFind
+{
+public:
+    UnionFind(int n)
+    {
+        p.resize(n);
+        std::iota(p.begin(), p.end(), 0);
+        rank.assign(n, 0);
+    }
+
+    int Find(int x) { return x == p[x] ? x : p[x] = Find(p[x]); }
+
+    void Union(int x, int y)
+    {
+        int xRoot = Find(x);
+        int yRoot = Find(y);
+
+        if (xRoot == yRoot)
+            return;
+
+        if (rank[xRoot] < rank[yRoot]) {
+            p[xRoot] = yRoot;
+        } else if (rank[xRoot] > rank[yRoot]) {
+            p[yRoot] = xRoot;
+        } else {
+            p[yRoot] = xRoot;
+            rank[xRoot]++;
+        }
+    }
+
+    bool sameUnion(int x, int y) { return Find(x) == Find(y); }
+
+private:
+    std::vector<int> p, rank;
+};
