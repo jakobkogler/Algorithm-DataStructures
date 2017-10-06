21c9e89a37072b58cb3ebea52d83dd9eb8282add
Jakob Kogler <jakob.kogler@gmail.com>
2017-10-06 18:29:00 +0200

    Variable max size for Factorial

--- Math/Factorial.h
+++ Math/Factorial.h
@@ -3,9 +3,9 @@
 int MOD = 1'000'000'009;
 std::vector<long long> factorial;
 
-void precompute_factorial()
+void precompute_factorial(int ma)
 {
     factorial.push_back(1);
-    for (int i = 1; i <= 5000; i++)
+    for (int i = 1; i <= ma; i++)
         factorial.push_back((factorial.back() * i) % MOD);
 }
