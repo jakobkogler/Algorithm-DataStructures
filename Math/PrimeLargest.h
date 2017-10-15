396dade8a1b51c4edd78ab864d18548c64268adf
Jakob Kogler <jakob.kogler@gmail.com>
2017-10-15 21:11:56 +0200

    Prime sieve that stores largest factor

---
+++ Math/PrimeLargest.h
@@ -0,0 +1,16 @@
+#include <numeric>
+#include <vector>
+
+void computePrimesLargest(int n, std::vector<int> &largest)
+{
+    largest.resize(n + 1);
+    std::iota(largest.begin(), largest.end(), 0);
+
+    for (int i = 2; i <= n; i++) {
+        if (i == largest[i]) {
+            for (int j = 2 * i; j <= n; j += i) {
+                largest[j] = i;
+            }
+        }
+    }
+}
