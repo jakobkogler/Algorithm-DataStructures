9615979c1d00d93ebc9f1eddbe03667af9fc0043
Jakob Kogler <jakob.kogler@gmail.com>
2017-12-05 12:32:08 +0100

    Make MOD changeable in power

--- Math/Exponentiation.h
+++ Math/Exponentiation.h
@@ -1,13 +1,13 @@
 int MOD = 1e9 + 7;
 
-long long power(long long base, int e)
+long long power(long long base, int e, int M=MOD)
 {
     long long result = 1;
-    base %= MOD;
+    base %= M;
     while (e) {
         if (e & 1)
-            result = (result * base) % MOD;
-        base = (base * base) % MOD;
+            result = (result * base) % M;
+        base = (base * base) % M;
         e >>= 1;
     }
     return result;
