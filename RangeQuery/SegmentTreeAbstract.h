f27a681ccb45a02d2eb2367cf318977615102226
Jakob Kogler <jakob.kogler@gmail.com>
2017-12-30 20:11:20 +0100

    Clean up SegmentTreeAbstract - "using" to make type shorter - replace tabs by spaces - fix error of initializing ret_l and ret_r

--- RangeQuery/SegmentTreeAbstract.h
+++ RangeQuery/SegmentTreeAbstract.h
@@ -5,45 +5,47 @@
 template<typename Segment>
 class SegmentTree {
 public:
-	SegmentTree(int count, std::function<Segment(Segment, Segment)> combine) : combine{combine} {
-		n = count;
-		data.resize(2 * n);
-	}
+    using CombineFkt = std::function<Segment(Segment, Segment)>;
 
-	SegmentTree(std::vector<Segment> const &values, std::function<Segment(Segment, Segment)> combine) : combine{combine} {
-		n = values.size();
-		data.resize(2 * n);
-    std::copy(values.begin(), values.end(), &data[0] + n);
-		for (int idx = n - 1; idx > 0; idx--)
-			data[idx] = combine(data[idx * 2], data[idx * 2 + 1]);
-	}
+    SegmentTree(int count, CombineFkt combine) : combine{combine} {
+        n = count;
+        data.resize(2 * n);
+    }
 
-	void update(int idx, Segment value) {
-		idx += n;
-		data[idx] = value;
+    SegmentTree(std::vector<Segment> const &values, CombineFkt combine) : combine{combine} {
+        n = values.size();
+        data.resize(2 * n);
+        std::copy(values.begin(), values.end(), &data[n]);
+        for (int idx = n - 1; idx > 0; idx--)
+            data[idx] = combine(data[idx * 2], data[idx * 2 + 1]);
+    }
 
-		while (idx > 1) {
-			idx /= 2;
-			data[idx] = combine(data[2 * idx], data[2 * idx + 1]);
-		}
-	}
+    void update(int idx, Segment value) {
+        idx += n;
+        data[idx] = value;
 
-	Segment minimum(int left, int right) { // interval [left, right)
-		left += n;
-		right += n;
-		Segment ret_l = data[left++], ret_r = data[--right];
+        while (idx > 1) {
+            idx /= 2;
+            data[idx] = combine(data[2 * idx], data[2 * idx + 1]);
+        }
+    }
 
-		while (left < right) {
-			if (left & 1) ret_l = combine(ret_l, data[left++]);
-			if (right & 1) ret_r = combine(data[--right], ret_r);
-			left >>= 1;
-			right >>= 1;
-		}
-		return combine(ret_l, ret_r);
-	}
+    Segment minimum(int left, int right) { // interval [left, right)
+        Segment ret_l{}, ret_r{};
+        left += n;
+        right += n;
+
+        while (left < right) {
+            if (left & 1) ret_l = combine(ret_l, data[left++]);
+            if (right & 1) ret_r = combine(data[--right], ret_r);
+            left >>= 1;
+            right >>= 1;
+        }
+        return combine(ret_l, ret_r);
+    }
 
 private:
-	int n;
-	std::vector<Segment> data;
-	std::function<Segment(Segment, Segment)> combine;
+    int n;
+    std::vector<Segment> data;
+    CombineFkt combine;
 };
