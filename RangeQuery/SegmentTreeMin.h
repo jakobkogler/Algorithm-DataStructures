fe37cdcecc4f41b2eee1c29bc5480075be2e34d9
Jakob Kogler <jakob.kogler@gmail.com>
2018-01-20 20:18:28 +0100

    spaces instead of tabs

--- RangeQuery/SegmentTreeMin.h
+++ RangeQuery/SegmentTreeMin.h
@@ -3,44 +3,44 @@
 
 class SegmentTree {
 public:
-	SegmentTree(int count) {
-		n = count;
-		data.assign(2 * n, 0);
-	}
+    SegmentTree(int count) {
+        n = count;
+        data.assign(2 * n, 0);
+    }
 
-	SegmentTree(std::vector<int> const &values) {
-		n = values.size();
-		data.resize(2 * n);
-		std::copy(values.begin(), values.end(), &data[0] + n);
-		for (int idx = n - 1; idx > 0; idx--)
-			data[idx] = std::min(data[idx * 2], data[idx * 2 + 1]);
-	}
+    SegmentTree(std::vector<int> const &values) {
+        n = values.size();
+        data.resize(2 * n);
+        std::copy(values.begin(), values.end(), &data[0] + n);
+        for (int idx = n - 1; idx > 0; idx--)
+            data[idx] = std::min(data[idx * 2], data[idx * 2 + 1]);
+    }
 
-	void update(int idx, int value) {
-		idx += n;
-		data[idx] = value;
+    void update(int idx, int value) {
+        idx += n;
+        data[idx] = value;
 
-		while (idx > 1) {
-			idx /= 2;
-			data[idx] = std::min(data[2 * idx], data[2 * idx + 1]);
-		}
-	}
+        while (idx > 1) {
+            idx /= 2;
+            data[idx] = std::min(data[2 * idx], data[2 * idx + 1]);
+        }
+    }
 
-	int minimum(int left, int right) { // interval [left, right)
-		int ret = std::numeric_limits<int>::max();
-		left += n;
-		right += n;
+    int minimum(int left, int right) { // interval [left, right)
+        int ret = std::numeric_limits<int>::max();
+        left += n;
+        right += n;
 
-		while (left < right) {
-			if (left & 1) ret = std::min(ret, data[left++]);
-			if (right & 1) ret = std::min(ret, data[--right]);
-			left >>= 1;
-			right >>= 1;
-		}
-		return ret;
-	}
+        while (left < right) {
+            if (left & 1) ret = std::min(ret, data[left++]);
+            if (right & 1) ret = std::min(ret, data[--right]);
+            left >>= 1;
+            right >>= 1;
+        }
+        return ret;
+    }
 
 private:
-	int n;
-	std::vector<int> data;
+    int n;
+    std::vector<int> data;
 };
