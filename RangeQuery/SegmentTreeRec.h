e8c09f1f94ba036cdb69a095e4b9bec5cbd18c4f
Jakob Kogler <jakob.kogler@gmail.com>
2018-01-22 13:40:51 +0100

    add build construction

--- RangeQuery/SegmentTreeRec.h
+++ RangeQuery/SegmentTreeRec.h
@@ -6,6 +6,27 @@ class SegmentTreeRec
 public:
     SegmentTreeRec(int n) : n(n) { data.assign(4 * n, 0); }
 
+    SegmentTreeRec(std::vector<int> const &values)
+    {
+        n = values.size();
+        data.assign(4 * n, 0);
+        build(values);
+    }
+
+    void build(std::vector<int> const &values, int id = 1, int l = 0, int r = -1) {
+        if (r == -1)
+            r = n;
+
+        if (l + 1 == r) {
+            data[id] = values[l];
+        } else {
+            int m = (l + r) >> 1;
+            build(values, id << 1, l, m);
+            build(values, id << 1 | 1, m, r);
+            data[id] = std::min(data[id << 1], data[id << 1 | 1]);
+        }
+    }
+
     int minimum(int x, int y, int id = 1, int l = 0, int r = -1)
     {
         if (r == -1)
