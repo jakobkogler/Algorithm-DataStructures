9fb65a093a3d37d4026950627705b684f5e95449
Jakob Kogler <jakob.kogler@gmail.com>
2018-04-12 01:13:07 +0200

    Modular Class

---
+++ Math/Modular.h
@@ -0,0 +1,74 @@
+#include <iostream>
+
+template <int MOD>
+class Modular {
+public:
+    Modular(long long v=0) {
+        value = v % MOD;
+        if (value < 0)
+            value += MOD;
+    }
+
+    Modular& operator+=(Modular const& b) {
+        value += b.value;
+        if (value >= MOD)
+            value -= MOD;
+        return *this;
+    }
+
+    Modular& operator-=(Modular const& b) {
+        value -= b.value;
+        if (value < 0)
+            value += MOD;
+        return *this;
+    }
+
+    Modular& operator*=(Modular const& b) {
+        value *= b.value;
+        value %= MOD;
+        return *this;
+    }
+
+    friend Modular power(Modular a, long long e) {
+        Modular res = 1;
+        while (e) {
+            if (e & 1)
+                res *= a;
+            a *= a;
+            e >>= 1;
+        }
+        return res;
+    }
+
+    friend Modular inverse(Modular a) {
+        return power(a, MOD - 2);
+    }
+
+    Modular& operator/=(Modular const& b) {
+        return *this *= inverse(b);
+    }
+
+    friend Modular operator+(Modular a, Modular const b) {
+        return a += b;
+    }
+
+    friend Modular operator-(Modular a, Modular const b) {
+        return a -= b;
+    }
+
+    friend Modular operator*(Modular a, Modular const b) {
+        return a *= b;
+    }
+
+    friend Modular operator/(Modular a, Modular const b) {
+        return a /= b;
+    }
+
+    friend std::ostream& operator<<(std::ostream &os, Modular const& a) {
+        return os << a.value;
+    }
+
+    long long value;
+};
+
+using ModInt = Modular<1'000'000'007>;
