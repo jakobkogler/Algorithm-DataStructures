dd6ae5525cf008ed9a0f41403987474b0850898f
Jakob Kogler <jakob.kogler@gmail.com>
2018-04-20 14:31:49 +0200

    faster FFT impl / easier to use

--- Math/FFT.h
+++ Math/FFT.h
@@ -1,107 +1,75 @@
 #include <vector>
 #include <complex>
 
-namespace FFT 
-{
+class FFT {
+public:
+    FFT(int max_degree) {
+        size = 1;
+        lg = 0;
+        while (size < max_degree + 1) {
+            size <<= 1;
+            lg++;
+        }
+
+        reverse.assign(size, 0);
+        for (int i = 1, j = 0; i < size; i++) {
+            int bit = size >> 1;
+            for (; j & bit; bit >>= 1)
+                j ^= bit;
+            j ^= bit;
+            reverse[i] = j;
+        }
+    }
+
     using cd = std::complex<double>;
     using vcd = std::vector<cd>;
 
-    void prepair(vcd& p) {
-        int n = 1;
-        while (n < (int)p.size()) {
-            n *= 2;
+    void fft(vcd & a, bool invert) {
+        for (int i = 0; i < size; i++) {
+            if (i < reverse[i])
+                swap(a[i], a[reverse[i]]);
         }
 
-        while ((int)p.size() < n) {
-            p.push_back(0);
-        }
-    }
-    
-    vcd fft(const vcd& as) {
-        int n = as.size();
-        int k = 0;
-        while ((1 << k) < n) k++;
-        std::vector<int> rev(n);
-        rev[0] = 0;
-        int high1 = -1;
-        for (int i = 1; i < n; i++) {
-            if ((i & (i - 1)) == 0)
-                high1++;
-            rev[i] = rev[i ^ (1 << high1)];
-            rev[i] |= 1 << (k - high1 - 1);
-        }
-
-        vcd roots(n);
-        for (int i = 0; i < n; i++) {
-            double alpha = 2 * M_PI * i / n;
-            roots[i] = cd(cos(alpha), sin(alpha));
-        }
-
-        vcd cur(n);
-        for (int i = 0; i < n; i++) {
-            cur[i] = as[rev[i]];
-        }
-
-        for (int len = 1; len < n; len <<= 1) {
-            vcd ncur(n);
-            int rstep = roots.size() / (len * 2);
-            for (int pdest = 0; pdest < n;) {
-                int p1 = pdest;
-                for (int i = 0; i < len; i++) {
-                    cd val = roots[i * rstep] * cur[p1 + len];
-                    ncur[pdest] = cur[p1] + val;
-                    ncur[pdest + len] = cur[p1] - val;
-                    pdest++, p1++;
+        for (int len = 1; len < size; len <<= 1) {
+            double ang = PI / len * (invert ? -1 : 1);
+            cd wlen(cos(ang), sin(ang));
+            for (int i = 0; i < size; i += 2*len) {
+                cd w(1);
+                for (int j = 0; j < len; j++) {
+                    cd u = a[i+j], v = a[i+j+len] * w;
+                    a[i+j] = u + v;
+                    a[i+j+len] = u - v;
+                    w *= wlen;
                 }
-                pdest += len;
             }
-            cur.swap(ncur);
         }
-        return cur;
+
+        if (invert) {
+            for (cd & x : a)
+                x /= size;
+        }
     }
 
-    vcd inverse_fft(const vcd& p) {
-        int n = p.size();
-        auto res = fft(p);
-        vcd inverse(n);
-        inverse[0] = res[0] / (double)n;
-        for (int i = 1; i < n; i++) {
-            inverse[i] = res[n - i] / (double)n;
-        }
-        return inverse;
+    std::vector<int> multiply(std::vector<int> const& a, std::vector<int> const& b) {
+        vcd fa(a.begin(), a.end()), fb(b.begin(), b.end());
+        fa.resize(size);
+        fb.resize(size);
+
+        fft(fa, false);
+        fft(fb, false);
+        for (int i = 0; i < size; i++)
+            fa[i] *= fb[i];
+        fft(fa, true);
+
+        std::vector<int> result(size);
+        for (int i = 0; i < size; i++)
+            result[i] = round(fa[i].real());
+        return result;
     }
 
-    vcd fft_rec(const vcd& as) {
-        int n = as.size();
-        if (n == 1) {
-            return as;
-        }
-
-        vcd P0(n / 2);
-        vcd P1(n / 2);
-        for (int i = 0; i < n / 2; i++) {
-            P0[i] = as[2 * i];
-            P1[i] = as[2 * i + 1];
-        }
-        auto S0 = fft(P0);
-        auto S1 = fft(P1);
-
-        cd step = {cos(2 * M_PI / n), sin(2 * M_PI / n)};
-        cd w = 1;
-        vcd answer(n);
-        for (int k = 0; k < n / 2; k++) {
-            answer[k] = S0[k] + w * S1[k];
-            answer[k + n / 2] = S0[k] - w * S1[k];
-            w *= step;
-        }
-        return answer;
-    }
-
-    vcd multiply(const vcd& a, const vcd& b) {
-        auto c = a;
-        for (int i = 0; i < (int)c.size(); i++) {
-            c[i] *= b[i];
-        }
-        return c;
-    }
+private:
+    int size;
+    int lg;
+    std::vector<int> reverse;
+    double PI = acos(-1);
 };
