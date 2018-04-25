95d11825bf3f72bba420677e7ac50ef94ff99cea
Jakob Kogler <jakob.kogler@gmail.com>
2018-04-25 22:57:52 +0200

    Add Trie for strings

---
+++ Trie/StringTrie.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <array>
+
+struct Vertex {
+    std::array<int, 26> next;
+    bool terminal = false;
+
+    Vertex() {
+        std::fill(next.begin(), next.end(), 0);
+    }
+};
+
+class Trie {
+public:
+    Trie() : t(1) {}
+
+    void add_string(std::string const& s) {
+        int v = 0;
+        for (char ch : s) {
+            int c = ch - 'a';
+            if (!t[v].next[c]) {
+                t[v].next[c] = t.size();
+                t.emplace_back();
+            }
+            v = t[v].next[c];
+        }
+        t[v].terminal = true;
+    }
+
+private:
+    std::vector<Vertex> t;
+};
