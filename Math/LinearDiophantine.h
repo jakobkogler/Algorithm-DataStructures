a50fc8ead3ec3bec476093f5ad45dc1c34b19405
Jakob Kogler <jakob.kogler@gmail.com>
2018-06-04 22:38:52 +0200

    solve linear diophantine equations https://codeforces.com/blog/entry/59842

---
+++ Math/LinearDiophantine.h
@@ -0,0 +1,60 @@
+template <typename T>
+class LinearDiophantine {
+public:
+    static T extended_gcd(T a, T b, T& x, T& y) {
+        if (a == 0) {
+            x = 0;
+            y = 1;
+            return b;
+        }
+        long long d = extended_gcd(b % a, a, y, x);
+        x -= b / a * y;
+        return d;
+    }
+
+    static bool solve(T a, T b, T c, T& x, T& y, T& g) {
+        if (a == 0) {
+            if (b == 0) {
+                x = y = 0;
+                g = 1;
+                return c == 0;
+            } else {
+                x = 0;
+                y = c / b;
+                g = abs(b);
+                return c % b == 0;
+            }
+        } else {
+            if (b == 0) {
+                x = c / a;
+                y = 0;
+                g = abs(b);
+                return c % a == 0;
+            } else {
+                g = extended_gcd(a, b, x, y);
+                T dx = c / a;
+                c %= a;
+                T dy = c / b;
+                c %= b;
+                x = dx + multiply_mod(x, c / g, b);
+                y = dy + multiply_mod(y, c / g, a);
+                g = abs(g);
+                return c % g == 0;
+            }
+        }
+    }
+
+    static T multiply_mod(T x, T y, T m) {
+        int sign = (x < 0 ? -1 : 1) * (y < 0 ? -1 : 1);
+        x = abs(x) % m;
+        y = abs(y) % m;
+        T res = 0;
+        while (y) {
+            if (y & 1)
+                res = (res + x) % m;
+            x = (x << 1) % m;
+            y >>= 1;
+        }
+        return sign * res;
+    }
+};
