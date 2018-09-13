2b7a68ed5c86fbae89b9de101bbd6cc9fce254e5
Alexander Enaldiev <Alexander.Enaldiev@kaspersky.com>
2018-09-13 13:36:26 +0300

    BinaryIndexedTree.cpp: let's fix the test's output

--- RangeQuery/BinaryIndexedTree.cpp
+++ RangeQuery/BinaryIndexedTree.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <iomanip>
 
 
 class BIT {
@@ -56,33 +57,45 @@ private:
 
 int main()
 {
-	std::vector<int> array{ 1, 7, 3, 0, 5, 8, 3, 2, 6, 2, 1, 1, 4, 5 };
+	std::vector<int> array{ 1, 7, 3, 0, 5,  8, 3, 2, 6, 2,  1, 1, 4, 5 };
 	BIT bit(array);
+	std::cout << "Index: ";
+	for (size_t ix = 1; ix <= array.size(); ++ix)
+	{
+		std::cout << ix << " ";
+	}
+	std::cout << std::endl;
 	std::cout << "Array: ";
-	for (int val : array) {
-		std::cout << val << " ";
+	for (size_t ix = 0; ix < array.size(); ++ix) {
+		std::cout << std::setw(ix >= 10? 2 : 0) << array[ix] << " ";
 	}
 	std::cout << std::endl << std::endl;
 
-	std::cout << "Prefix sum of first 13 elements: " << bit.prefix_query(12) << std::endl;
-	std::cout << "Prefix sum of first 7 elements: " << bit.prefix_query(6) << std::endl;
-	std::cout << "Range sum from pos 1 to pos 5: " << bit.range_query(1, 5) << std::endl;
+	std::cout << "Prefix sum of first 13 elements: \t" << bit.prefix_query(12) << std::endl;
+	std::cout << "Prefix sum of first 7 elements: \t" << bit.prefix_query(6) << std::endl;
+	std::cout << "Range sum from pos 1 to pos 5: \t\t" << bit.range_query(1, 5) << std::endl;
 	std::cout << std::endl;
 
 	bit.update(4, 2);
-	std::cout << "Add 2 to element at pos 4" << std::endl;
+	std::cout << "Add 2 to element at pos 5" << std::endl;
 	std::vector<int> new_array;
 	for (int idx = 0; idx < array.size(); idx++) {
 		new_array.push_back(bit.range_query(idx, idx));
 	}
+	std::cout << "Index: ";
+	for (size_t ix = 1; ix <= new_array.size(); ++ix)
+	{
+		std::cout << ix << " ";
+	}
+	std::cout << std::endl;
 	std::cout << "Array: ";
-	for (int val : array) {
-		std::cout << val << " ";
+	for (size_t ix = 0; ix < new_array.size(); ++ix) {
+		std::cout << std::setw(ix >= 10 ? 2 : 0) << new_array[ix] << " ";
 	}
 	std::cout << std::endl << std::endl;
 
-	std::cout << "Prefix sum of first 13 elements: " << bit.prefix_query(12) << std::endl;
-	std::cout << "Prefix sum of first 7 elements: " << bit.prefix_query(6) << std::endl;
-	std::cout << "Range sum from pos 1 to pos 5: " << bit.range_query(1, 5) << std::endl;
+	std::cout << "Prefix sum of first 13 elements: \t" << bit.prefix_query(12) << std::endl;
+	std::cout << "Prefix sum of first 7 elements: \t" << bit.prefix_query(6) << std::endl;
+	std::cout << "Range sum from pos 1 to pos 5: \t\t" << bit.range_query(1, 5) << std::endl;
 	std::cout << std::endl;
 }						
