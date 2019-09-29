f96d0da3d018caf2da63fe26f9cd751294a31b1c
Madhur Chauhan <madhur4127@gmail.com>
2019-09-29 09:15:02 +0530

    is there a reason for copying elements in the constructor?

--- RangeQuery/BIT.h
+++ RangeQuery/BIT.h
@@ -4,14 +4,12 @@
 class BIT
 {
 public:
-    BIT(std::vector<int> list)
+    BIT(const std::vector<int>& list)
     {
         // Initialize BIT with list in O(n)
         m_array = std::vector<int>(list.size() + 1, 0);
-        for (int idx = 0; idx < (int)list.size(); idx++) {
-            m_array[idx + 1] = list[idx];
-        }
-
+        std::copy(list.begin(), list.end(), m_array.begin() + 1);
+        
         for (int idx = 1; idx < (int)m_array.size(); idx++) {
             int idx2 = idx + (idx & -idx);
             if (idx2 < (int)m_array.size()) {
