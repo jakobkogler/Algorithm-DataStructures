67c569acdfbdca70d25fc558f513150d5ce04ccd
Jakob Kogler <jakob.kogler@gmail.com>
2021-04-17 13:20:26 +0200

    template for BIT

--- RangeQuery/BIT.h
+++ RangeQuery/BIT.h
@@ -1,47 +1,48 @@
 #include <vector>
 
+template<typename T = long long>
+class BIT {
 // Implementation of a Binary Indexed Tree (Fennwick Tree)
-class BIT
-{
 public:
-    BIT(const std::vector<int>& list)
-    {
+    BIT(int n) : n(n + 1) {
+        m_array = std::vector<T>(this->n, 0);
+    }
+
+    BIT(const std::vector<T>& list) {
         // Initialize BIT with list in O(n)
-        m_array = std::vector<int>(list.size() + 1, 0);
+        m_array = std::vector<T>(n, 0);
         std::copy(list.begin(), list.end(), m_array.begin() + 1);
         
-        for (int idx = 1; idx < (int)m_array.size(); idx++) {
+        for (int idx = 1; idx < n; idx++) {
             int idx2 = idx + (idx & -idx);
-            if (idx2 < (int)m_array.size()) {
+            if (idx2 < n) {
                 m_array[idx2] += m_array[idx];
             }
         }
     }
 
-    int prefix_query(int idx)
-    {
-        // Computes prefix sum of up to including the idx-th element
+    // Computes sum of the range [0, idx)
+    int prefix_query(int idx) {
         int result = 0;
-        for (++idx; idx > 0; idx -= idx & -idx) {
+        for (; idx > 0; idx -= idx & -idx) {
             result += m_array[idx];
         }
         return result;
     }
 
-    int range_query(int from_idx, int to_idx)
-    {
-        // Computes the range sum between two indices (both inclusive)
-        return prefix_query(to_idx) - prefix_query(from_idx - 1);
+    // Computes the range sum of the range [l, r)
+    int range_query(int l, int r) {
+        return prefix_query(r) - prefix_query(l);
     }
 
-    void update(int idx, int add)
-    {
-        // Add a value to the idx-th element
-        for (++idx; idx < (int)m_array.size(); idx += idx & -idx) {
+    // Add a value to the idx-th element
+    void update(int idx, int add) {
+        for (++idx; idx < n; idx += idx & -idx) {
             m_array[idx] += add;
         }
     }
 
 private:
-    std::vector<int> m_array;
+    std::vector<T> m_array;
+    int n;
 };
