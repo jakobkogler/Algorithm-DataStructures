f8086642b40cc15f20fbb08fba5ded567773e116
Jakob Kogler <jakob.kogler@gmail.com>
2023-01-15 12:08:38 +0100

    fix negative divisor case for ceil/floor division

--- Math/FloorCeilDivision.h
+++ Math/FloorCeilDivision.h
@@ -1,9 +1,17 @@
 template <typename T>
 T floor_div(T a, long long b) {
+    if (b < 0) {
+        a = -a;
+        b = -b;
+    }
     return (a < 0 ? a - (b - 1) : a) / b;
 }
 
 template <typename T>
 T ceil_div(T a, long long b) {
+    if (b < 0) {
+        a = -a;
+        b = -b;
+    }
     return (a >= 0 ? a + (b - 1) : a) / b;
 }
